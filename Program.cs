f7d594e3303ba5214cdf882c713d0e465912ac80
Hafiz Hamdard <ahmadshakhi2020@gmail.com>
2024-01-16 17:47:24 -0800

    created new examples & fixed the problem

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿namespace DeclaringMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Personalized Poem Generator");
+
+            Console.WriteLine("Enter your name: ");
+            string UserName = Console.ReadLine();
+
+            Console.WriteLine("Enter your color:  ");
+            string UserColor = Console.ReadLine();
+
+            Console.WriteLine("Enter an emotion:  ");
+            string UserEmotion = Console.ReadLine();
+
+            Console.WriteLine("Enter a place you like : ");
+            string UserPlace = Console.ReadLine();
+
+            Console.WriteLine("/n ---------HERE IS YOUR PERSONALIZED POEM-------");
+
+            Console.WriteLine($"{UserName} with eyes as {UserColor} as the sky\n");
+            Console.WriteLine($"in the realm of {UserPlace} where {UserEmotion} fly");
+            Console.WriteLine($"Feeling {UserEmotion}, a tale to unfold");
+            Console.WriteLine($"in the land where dreams take flight A journey begins for {UserName} in the soft moonlight");
+            Console.WriteLine($"{UserName} feels {UserEmotion}, a heart's gentle song,");
+            Console.WriteLine($"{UserName} with eyes as {UserColor} as the sky");
+            Console.WriteLine($"in the realm of {UserPlace} where {UserEmotion} fly");
+            Console.WriteLine($"in the land where dreams take flight A journey begins for {UserName} in the soft moonlight");
+
+            Console.WriteLine(" Thanks for using personlized peom\n");
+
+        }
+    }
+    }
---
+++ Userinput/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Userinput
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name, language, position;
+            Console.WriteLine("what is your name:  ?");
+            name = Console.ReadLine();
+            Console.WriteLine("what programing language do you know: ?");
+            language = Console.ReadLine();
+            Console.WriteLine("what is your position: ?");
+            position = Console.ReadLine();
+
+            Console.WriteLine($"Why did {name} choose {language}? Because they wanted to be a {position}!\n");
+
+
+
+
+
+
+            double sum = Multiply(3, 4, 22, 33, 4, 1);
+            Console.WriteLine(sum);
+        }
+        public static double Multiply(params double[] prices)
+        {
+            double Total = 0;
+            {
+                foreach (var pr in prices)
+                {
+                    Total += pr;
+                }
+            }
+            return Total;
+        }
+    }
+}
